05c8c33ce0f7e2fde03999bcabf66f12e5709d4b
KyryloChernuskyi <k.chernusskiy@gmail.com>
2021-03-12 12:19:39 +0200

    second task v.2.0

--- task_2.js
+++ task_2.js
@@ -3,51 +3,32 @@ const users = [
   {name: "Pete", surname: "Hunt", id: 2,},
   {name: "Mary", surname: "Key", id: 3,}
 ];
-const usersMapped = [];
+
+//----------------------------------------------------------------------------
 
 // 1st variant
 
-// let item;
-// function mapUsers (arr1, arr2) {
-//   for (i = 0; i < arr1.length; i++) {
-//     item = arr1[i];
-//     arr2.push({'fullname' : `${item.name} ${item.surname}`, 'id':item.id})
-//     //arr2[i] = new Mapping (arr1[i].name + ' ' + arr1[i].surname, arr1[i].id)
-//   }
-// }
+let item;
+function mapUsers (arr1) {
+  const arr2 = [];
+  for (i = 0; i < arr1.length; i++) {
+    item = arr1[i];
+    arr2.push({'Full Name' : `${item.name} ${item.surname}`, 'id':item.id})
+  }
+  return arr2;
+}
+
+const usersMapped_1 = mapUsers(users);
 
 //----------------------------------------------------------------------------
 
 //2nd variant
 
-// function Mapping (fullname, id) {
-//   this.fullname = fullname;
-//   this.id = id;
-//   return {fullname, id};
-// }
-//
-// function mapUsers (arr1, arr2) {
-//   for (i = 0; i < arr1.length; i++) {
-//     arr2[i] = new Mapping (arr1[i].name + ' ' + arr1[i].surname, arr1[i].id)
-//   }
-//   return arr2;
-// }
+const usersMapped_2 = users.map(function (i) { return {fullName: `${i.name} ${i.surname}`, id: i.id} })
 
 //----------------------------------------------------------------------------
 
-//3rd variant
-
-function mapUsers (arr1, arr2) {
-  for (i = 0; i < arr1.length; i++) {
-    arr2 = arr1.map(function (fullName, id) {
-      fullname: `${arr1[i].name} ${arr1[i].surname}`,
-      id: `${arr1[i].id}`,
-    })
-  }
-  return arr2;
-}
-
-//----------------------------------------------------------------------------
-
-mapUsers(users, usersMapped);
-console.log (usersMapped)
+console.log ('Option 1:');
+console.log (usersMapped_1);
+console.log ('Option 2:');
+console.log (usersMapped_2);
