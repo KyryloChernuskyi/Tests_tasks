5e9d4d9e844b20888240033799c48ccc505afb50
KyryloChernuskyi <k.chernusskiy@gmail.com>
2021-03-12 10:12:51 +0200

    first task v.1.2

--- task_1.js
+++ task_1.js
@@ -1,7 +1,7 @@
 let arr1 = [2, 4, 1, -5, 54, 32, 14];
+let min = arr1[0];
 
 function findMin (arr) {
-  let min = arr[0];
   for (let i = 1; i < arr.length; i++) {
     if (min > arr[i]) {
       min = arr[i];
@@ -10,5 +10,5 @@ function findMin (arr) {
   return min;
 }
 
-console.log ('Min of ', arr, 'is ', min )
 findMin(arr1);
+console.log ('Min of ', arr1, 'is ', min )
